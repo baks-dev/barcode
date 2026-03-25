428a60f64f32af8d04ba9be1006d7e166587ae0e
MichelAngelo <baksdevelopmen@gmail.com>
2026-03-26 01:06:54 +0300

    BaksDevelopment

--- Security/VoterScanner.php
+++ Security/VoterScanner.php
@@ -25,7 +25,6 @@ declare(strict_types=1);
 
 namespace BaksDev\Barcode\Security;
 
-use BaksDev\Contacts\Region\Security\Role;
 use BaksDev\Users\Profile\Group\Security\RoleInterface;
 use BaksDev\Users\Profile\Group\Security\VoterInterface;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
